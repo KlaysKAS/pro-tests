92a76d4468b9e94c8210f8f138865fd846681c1a
zZzwat4er <valenten2001@mail.ru>
2023-04-13 20:32:48 +0300

    Add app themes

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,9 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import 'l10n/all_locales.dart';
 
+import 'ui/theme/themes.dart';
+
+
 void main() {
   runApp(const MyApp());
 }
@@ -24,18 +27,9 @@ class MyApp extends StatelessWidget {
         GlobalWidgetsLocalizations.delegate,
       ],
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+      themeMode: ThemeMode.dark,
+      theme: AppThemes.lightTheme,
+      darkTheme: AppThemes.darkTheme,
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
