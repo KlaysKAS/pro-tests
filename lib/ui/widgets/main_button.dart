4deb8829714e83d124c8d16dfb7467d05ab8b0b9
khudysh <ehd2001@mail.ru>
2023-04-14 22:50:17 +0300

    Added MainButton widget

---
+++ lib/ui/widgets/main_button.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:pro_tests/ui/theme/const.dart';
+
+class MainButton extends StatelessWidget {
+  final String btnText;
+  final void Function()? onPressed;
+  const MainButton({Key? key, required this.btnText, required this.onPressed})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+          horizontal: Const.paddingBetweenLarge),
+      child: Row(
+        children: [
+          Expanded(
+            child: ElevatedButton(
+              onPressed: onPressed,
+              child: Padding(
+                padding: const EdgeInsets.all(22),
+                child: Text(btnText,
+                    style: const TextStyle(fontSize: Const.fontSizeButton)),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
