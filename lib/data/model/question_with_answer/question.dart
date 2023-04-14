68c984531d767ed4b9ca150653f75f51128d6532
Kirill Dolmatov <dolmatov.kd@dns-shop.ru>
2023-04-14 12:53:03 +0300

    Change answer param to nullable type

---
+++ lib/data/model/question_with_answer/question.dart
@@ -0,0 +1,17 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'question.freezed.dart';
+
+part 'question.g.dart';
+
+@freezed
+class Question with _$Question {
+  const factory Question({
+    required int id,
+    required String question,
+    required List<String> answers,
+    required String? answer,
+  }) = _Question;
+
+  factory Question.fromJson(Map<String, Object?> json) => _$QuestionFromJson(json);
+}
