488e350d652e8d859d255c2191a08fadc2b90996
Kirill Dolmatov <dolmatov.kd@dns-shop.ru>
2023-04-14 12:53:03 +0300

    Change answer param to nullable type

--- lib/data/model/question_with_answer/question.g.dart
+++ lib/data/model/question_with_answer/question.g.dart
@@ -1,23 +1,20 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'question_with_answer.dart';
+part of 'question.dart';
 
 // **************************************************************************
 // JsonSerializableGenerator
 // **************************************************************************
 
-_$_QuestionWithAnswer _$$_QuestionWithAnswerFromJson(
-        Map<String, dynamic> json) =>
-    _$_QuestionWithAnswer(
+_$_Question _$$_QuestionFromJson(Map<String, dynamic> json) => _$_Question(
       id: json['id'] as int,
       question: json['question'] as String,
       answers:
           (json['answers'] as List<dynamic>).map((e) => e as String).toList(),
-      answer: json['answer'] as String,
+      answer: json['answer'] as String?,
     );
 
-Map<String, dynamic> _$$_QuestionWithAnswerToJson(
-        _$_QuestionWithAnswer instance) =>
+Map<String, dynamic> _$$_QuestionToJson(_$_Question instance) =>
     <String, dynamic>{
       'id': instance.id,
       'question': instance.question,
---
+++ test/models_test/question_test.dart
@@ -0,0 +1,85 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:pro_tests/data/model/question_with_answer/question.dart';
+
+void main() {
+  group('Question model testing', () {
+    const id = 0;
+    const question = 'a';
+    final answers = ['a', 'b', 'c', 'd'];
+    const answer = 'b';
+
+    test('Test factory method', () {
+      final questionO = Question(id: id, question: question, answers: answers, answer: answer);
+
+      expect(questionO.id, id);
+      expect(questionO.question, question);
+      expect(questionO.answers, answers);
+      expect(questionO.answer, answer);
+
+      final questionONull = Question(id: id, question: question, answers: answers, answer: null);
+      expect(questionONull.answer, null);
+    });
+
+    test('Test from json', () {
+      final json = <String, Object>{
+        'id': id,
+        'question': question,
+        'answers': answers,
+        'answer': answer,
+      };
+      final questionO = Question.fromJson(json);
+      expect(questionO.id, id);
+      expect(questionO.question, question);
+      expect(questionO.answers, answers);
+      expect(questionO.answer, answer);
+
+      final jsonNull = json..remove('answer');
+      final questionNull = Question.fromJson(jsonNull);
+      expect(questionNull.answer, null);
+    });
+
+    test('Test to json', () {
+      final questionO = Question(id: id, question: question, answers: answers, answer: answer);
+      final questionNull = Question(id: id, question: question, answers: answers, answer: null);
+      final json = questionO.toJson();
+      final jsonNull = questionNull.toJson();
+      final questionOb = Question.fromJson(json);
+      final questionNullO = Question.fromJson(jsonNull);
+
+      expect(questionO.id, questionOb.id);
+      expect(questionO.question, questionOb.question);
+      expect(questionO.answers, questionOb.answers);
+      expect(questionO.answer, questionOb.answer);
+
+      expect(questionNull.id, questionNullO.id);
+      expect(questionNull.question, questionNullO.question);
+      expect(questionNull.answers, questionNullO.answers);
+      expect(questionNull.answer, questionNullO.answer);
+    });
+
+    test('Test equals', () {
+      final newAnswers = ['a'];
+      final question1 = Question(id: id, question: question, answers: answers, answer: answer);
+      final question2 = Question(id: id, question: question, answers: answers, answer: answer);
+      final question3 = Question(id: 2, question: '', answers: newAnswers, answer: null);
+      final question4 = Question(id: 2, question: '', answers: newAnswers, answer: null);
+      expect(question1 == question2, true);
+      expect(question1.id == question2.id, true);
+      expect(question1.question == question2.question, true);
+      expect(question1.answers == question2.answers, true);
+      expect(question1.answer == question2.answer, true);
+
+      expect(question1 == question3, false);
+      expect(question1.id == question3.id, false);
+      expect(question1.question == question3.question, false);
+      expect(question1.answers == question3.answers, false);
+      expect(question1.answer == question3.answer, false);
+
+      expect(question3 == question4, true);
+      expect(question3.id == question4.id, true);
+      expect(question3.question == question4.question, true);
+      expect(question3.answers == question4.answers, true);
+      expect(question3.answer == question4.answer, true);
+    });
+  });
+}
