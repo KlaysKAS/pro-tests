3a981d5b41acc17044c0e89c84a7c56ce9b86cb4
zZzwat4er <valenten2001@mail.ru>
2023-04-14 17:01:51 +0300

    Add dark check box theme

--- lib/ui/theme/check_box/check_box.dart
+++ lib/ui/theme/check_box/check_box.dart
@@ -4,10 +4,20 @@ import '../const.dart';
 
 class CheckBoxThemes {
   static final main = CheckboxThemeData(
-    checkColor: MaterialStatePropertyAll(Const.secondaryColor),
+    checkColor: const MaterialStatePropertyAll(Const.secondaryColor),
     fillColor: MaterialStateProperty.resolveWith((state) {
       if (state.contains(MaterialState.selected)) {
         return Const.primaryColor;
+      }
+      return Const.inactiveColor;
+    }),
+  );
+
+  static final dark = CheckboxThemeData(
+    checkColor: const MaterialStatePropertyAll(Const.darkSecondaryColor),
+    fillColor: MaterialStateProperty.resolveWith((state) {
+      if (state.contains(MaterialState.selected)) {
+        return Const.darkPrimaryColor;
       }
       return Const.inactiveColor;
     }),
