da09757539a3a001318b5372005c52fa6bc0ad04
khudysh <ehd2001@mail.ru>
2023-04-15 22:06:40 +0300

    Added dispose, extract widget

--- lib/ui/screens/auth/login.dart
+++ lib/ui/screens/auth/login.dart
@@ -4,17 +4,8 @@ import 'package:pro_tests/ui/theme/const.dart';
 import 'package:pro_tests/ui/widgets/main_button.dart';
 import 'package:pro_tests/ui/widgets/main_form_input.dart';
 
-class LoginScreen extends StatefulWidget {
+class LoginScreen extends StatelessWidget {
   LoginScreen({super.key});
-
-  @override
-  State<LoginScreen> createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
-  final TextEditingController _loginFormController = TextEditingController();
-  final TextEditingController _passwordFormController = TextEditingController();
-  bool _hidePass = false;
 
   @override
   Widget build(BuildContext context) {
@@ -28,34 +19,59 @@ class _LoginScreenState extends State<LoginScreen> {
             style: const TextStyle(fontSize: Const.fontSizeBodyTitle),
           ),
           const SizedBox(height: Const.paddingBetweenLarge * 3),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(height: Const.paddingBetweenLarge),
-              MainFormInput(
-                controller: _loginFormController,
-                onChanged: (value) => {_loginFormController.text = value},
-                decoration: InputDecoration(label: Text(AppLocalizations.of(context)!.loginFormMailTitle)),
-              ),
-              const SizedBox(height: Const.paddingBetweenLarge),
-              MainFormInput(
-                obscureText: _hidePass,
-                controller: _passwordFormController,
-                onChanged: (value) => {_passwordFormController.text = value},
-                decoration: InputDecoration(
-                    suffixIcon: GestureDetector(
-                      onTap: () => {_hidePass = !_hidePass, setState(() => {})},
-                      child: Icon(_hidePass ? Icons.visibility : Icons.visibility_off),
-                    ),
-                    label: Text(AppLocalizations.of(context)!.loginFormPasswordTitle)),
-              ),
-            ],
-          ),
+          _Content(),
           const Expanded(child: SizedBox()),
           MainButton(btnText: AppLocalizations.of(context)!.loginBtn, onPressed: () => {}),
           const SizedBox(height: Const.paddingBetweenLarge),
         ],
       ),
+    );
+  }
+}
+
+class _Content extends StatefulWidget {
+  @override
+  State<_Content> createState() => _ContentState();
+}
+
+class _ContentState extends State<_Content> {
+  final TextEditingController _loginFormController = TextEditingController();
+
+  final TextEditingController _passwordFormController = TextEditingController();
+
+  bool _hidePass = false;
+
+  @override
+  void dispose() {
+    _loginFormController.dispose();
+    _passwordFormController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const SizedBox(height: Const.paddingBetweenLarge),
+        MainFormInput(
+          controller: _loginFormController,
+          onChanged: (value) => {_loginFormController.text = value},
+          decoration: InputDecoration(label: Text(AppLocalizations.of(context)!.loginFormMailTitle)),
+        ),
+        const SizedBox(height: Const.paddingBetweenLarge),
+        MainFormInput(
+          obscureText: _hidePass,
+          controller: _passwordFormController,
+          onChanged: (value) => {_passwordFormController.text = value},
+          decoration: InputDecoration(
+              suffixIcon: GestureDetector(
+                onTap: () => {_hidePass = !_hidePass, setState(() => {})},
+                child: Icon(_hidePass ? Icons.visibility : Icons.visibility_off),
+              ),
+              label: Text(AppLocalizations.of(context)!.loginFormPasswordTitle)),
+        ),
+      ],
     );
   }
 }
