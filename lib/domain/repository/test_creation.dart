a4d5f829647e0aa1f4779cf18ac889e39de834aa
Nerlih Max <nerlihmax@yandex.ru>
2023-04-18 15:14:54 +0300

    chore: lints ingores

--- lib/domain/repository/test_creation.dart
+++ lib/domain/repository/test_creation.dart
@@ -1,8 +1,11 @@
 abstract class TestCreationRepository {
+  // ignore: undefined_class
   TestInfo createTest(String name, String description);
 
+  // ignore: undefined_class
   TestInfo addQuestion(String testId, Question question);
 
+// ignore: non_type_as_type_argument
   List<TestInfo> getUsersTests(String userId);
 
   String deleteTest(String testId);
