d6e7fd1df415d435de9e6d9b50faae89791dd907
Kirill Dolmatov <dolmatov.kd@dns-shop.ru>
2023-04-18 16:52:36 +0300

    Set auth page after splash

--- lib/ui/screens/splash/splash.dart
+++ lib/ui/screens/splash/splash.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:pro_tests/ui/router/router.dart';
+import 'package:pro_tests/ui/router/routes.dart';
 
 class SplashScreenScreen extends StatelessWidget {
   const SplashScreenScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    WidgetsFlutterBinding.ensureInitialized().addPostFrameCallback((timeStamp) {
+      _pushAuth();
+    });
     return const Placeholder();
+  }
+
+  void _pushAuth() {
+    AppRouter.router.replaceNamed(AppRoutes.auth.name);
   }
 }
