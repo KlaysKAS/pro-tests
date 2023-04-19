8c0c80ddc2ef2a8814306d94eda2054bad49975d
Kirill Dolmatov <dolmatov.kd@dns-shop.ru>
2023-04-20 00:34:35 +0300

    Add init states

--- lib/domain/providers/service_locator.dart
+++ lib/domain/providers/service_locator.dart
@@ -1,7 +1,11 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pro_tests/domain/models/test_lists/test_lists.dart';
+import 'package:pro_tests/domain/models/test_with_question/test_with_questions.dart';
 import 'package:pro_tests/domain/providers/auth_state.dart';
+import 'package:pro_tests/domain/providers/test_creation.dart';
+import 'package:pro_tests/domain/providers/test_list.dart';
 import 'package:pro_tests/domain/repository/token_manager/token_manager.dart';
 import 'package:pro_tests/ui/states/authentication_state/authentication_state.dart';
 
@@ -14,8 +18,8 @@ abstract class ServiceLocator {
   abstract final StateNotifierProvider<AuthenticationStateNotifier, AuthenticationState> authenticationStateNotifier;
 
 // abstract final TestAttemptStateNotifier testAttemptStateNotifier;
-// abstract final TestCreationStateNotifier testCreationStateNotifier;
-// abstract final TestListStateNotifier testListStateNotifier;
+  abstract final StateNotifierProvider<TestCreationStateNotifier, TestWithQuestion> testCreationStateNotifier;
+  abstract final StateNotifierProvider<TestListStateNotifier, TestLists> testListStateNotifier;
 
-  Future<bool> init();
+  Future<void> init();
 }
