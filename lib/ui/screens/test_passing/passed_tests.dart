7278025247d16171064d8245ff9fa4b57b91794e
khudysh <ehd2001@mail.ru>
2023-04-19 10:18:00 +0300

    Fix hardcode to const

--- lib/ui/screens/test_passing/passed_tests.dart
+++ lib/ui/screens/test_passing/passed_tests.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pro_tests/ui/theme/const.dart';
 import 'package:pro_tests/ui/widgets/modal_qr_link.dart';
 
 class PassedTestScreen extends StatelessWidget {
@@ -15,7 +16,8 @@ class PassedTestScreen extends StatelessWidget {
                 onPressed: () {
                   showModalBottomSheet<void>(
                       shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10)),
+                        borderRadius: BorderRadius.only(
+                            topLeft: Const.borderRadiusStandart, topRight: Const.borderRadiusStandart),
                       ),
                       context: context,
                       builder: (BuildContext context) {
