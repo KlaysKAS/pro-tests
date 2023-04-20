199110117550f4c2851552f73d92d2ebfaff746d
Kirill Dolmatov <dolmatov.kd@dns-shop.ru>
2023-04-20 11:18:08 +0300

    Create interface for service
    create data model for answer

---
+++ lib/data/model/answer/answer.dart
@@ -0,0 +1,16 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'answer.freezed.dart';
+
+part 'answer.g.dart';
+
+@freezed
+class Answer with _$Answer {
+  const factory Answer({
+    required final String title,
+    required final String answer,
+    final bool? right,
+  }) = _Answer;
+
+  factory Answer.fromJson(Map<String, dynamic> json) => _$AnswerFromJson(json);
+}
